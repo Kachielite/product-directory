3ea468152add372ea14b200636c52b2328642e6d
Kachielite <40024356+Kachielite@users.noreply.github.com>
2024-01-04 05:33:21 +0100

    added ProductDAO implementer

---
+++ src/main/java/com/derrick/inventory/dao/ProductDAOImpl.java
@@ -0,0 +1,43 @@
+package com.derrick.inventory.dao;
+
+import com.derrick.inventory.entity.Product;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductDAOImpl implements ProductDAO{
+    // define variable for entity manager
+    EntityManager entityManager;
+
+    // construction injection
+    @Autowired
+    public ProductDAOImpl(EntityManager theEntityManager){
+        entityManager = theEntityManager;
+    }
+
+    @Override
+    public Product save(Product product) {
+        return entityManager.merge(product);
+    }
+
+    @Override
+    public List<Product> findAll() {
+        TypedQuery<Product> theQuery = entityManager.createQuery("From products", Product.class);
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public Product findById(int id) {
+        return entityManager.find(Product.class, id);
+    }
+
+    @Override
+    public void deleteById(int id) {
+        Product product = entityManager.find(Product.class, id);
+        entityManager.remove(product);
+    }
+}
